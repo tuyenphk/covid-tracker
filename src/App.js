0e72c5d0a9184f9e5fc395744f742f9787917d54
Tuyen Pham <tuh39709@temple.edu>
2021-03-08 23:46:28 -0500

    fix import wrong folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cards from './components/Cards/Cards'
 import Chart from './components/Chart/Chart'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 
-import {fetchData} from './api/index';
+import {fetchData} from './api/index'
 
 import styles from './App.module.css'
 class App extends Component{
